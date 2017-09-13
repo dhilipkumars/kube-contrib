f12bffff8a57ace1b9f8a25e14d85ac1c7b34e5c
Dhilip <dhilip.kumar.s@huawei.com>
2017-09-13 15:05:05 +0530

    Calculate commtis for all the Huawei-Paas members

--- contrib.go
+++ contrib.go
@@ -1,42 +1,37 @@
 package main
 
-import
-(
-	"fmt"
+import (
 	"context"
+	"fmt"
 	"github.com/google/go-github/github"
-	"strings"
-	"time"
-	"os"
-	"sync"
 	"log"
+	"sync"
+	"time"
+	"sort"
 )
 
-var Organizations = []string {"kubernetes", "kubernetes-incubator"}
-var Users = []string { "dhilipkumars" }
+var Organizations = []string{"kubernetes", "kubernetes-incubator"}
+//var Organizations = []string{"kubernetes"}
 var ctx context.Context
 var cli *github.Client
 var core *GitWorker
 var search *GitWorker
 
+const (
+	UserSyncPeriod   = 4
+	CommitSyncPeriod = 1
+	PRSyncPeriod     = 1
+	ItemsPerPage     = 1000
+	HuaweiOrg        = "Huawei-PaaS"
+)
+
 type Table struct {
-	sync.Mutex					//Lock
-	Row map[string]*Record 		//Table to be populated
-	RemaingCalls 	int			//Remain number of HTTP calls this we are allowed to make for this client connection
-	ResetTime		time.Time	//If we ran out of the HTTP calls for this hour, when does it reset?
-	Update time.Time			//Last updated time for commits
+	sync.Mutex                    //Lock
+	Row        map[string]*Record //Table to be populated
+	Update     time.Time          //Last updated time for commits
 }
 
-type Record struct {
-	sync.Mutex
-	Name string 		//Name of the person
-	GHandle string		//Github handle of this person
-	Commits int			//Number of commits so far
-	NewCommits int		//Current processing number of commits
-	Updated time.Time   //Last updated at
-}
-
-func (T *Table) Add (rec *Record) error {
+func (T *Table) Add(rec *Record) error {
 
 	T.Lock()
 	defer T.Unlock()
@@ -52,21 +47,27 @@ func (T *Table) Exist(gHandle string) bool {
 	return ok
 }
 
-func (T *Table) zeroCommits () {
+func (T *Table) zeroCommits() {
 
 	for _, val := range T.Row {
 		val.NewCommits = 0
 	}
 }
 
-func (T *Table) swapToCommits () {
+func (T *Table) swapToCommits() {
 
 	for _, val := range T.Row {
 		val.Commits = val.NewCommits
 	}
 }
 
-func (T *Table) Tabulate () []Record {
+type ByCommit []Record
+
+func (b ByCommit) Len() int {return len(b)}
+func (b ByCommit) Swap(i,j int) {b[i], b[j] = b[j], b[i]}
+func (b ByCommit) Less(i, j int) bool {return b[i].Commits < b[j].Commits}
+
+func (T *Table) Tabulate() []Record {
 
 	var result []Record
 
@@ -74,85 +75,188 @@ func (T *Table) Tabulate () []Record {
 		result = append(result, *val)
 	}
 
+	sort.Sort(ByCommit(result))
+
 	return result
 }
 
+//Sync syncs the the table and update it.
+func (T *Table) Sync() error {
+	UserCh := time.After(time.Second * 10)
+	CommitCh := time.After(time.Minute * CommitSyncPeriod)
+	PRCh := time.After(time.Minute * PRSyncPeriod)
+
+	for {
+		select {
+
+		case <-UserCh:
+			T.SyncUser()
+			UserCh = time.After(time.Hour * UserSyncPeriod)
+			log.Printf("Table->Sync() Synced Users")
+
+		case <-CommitCh:
+			T.SyncCommits()
+			log.Printf("Table->Sync() Synced Commits")
+			CommitCh = time.After(time.Hour * CommitSyncPeriod)
+
+		case <-PRCh:
+			//T.SyncPRs()
+			log.Printf("Table->Sync() Synced PRs")
+			PRCh = time.After(time.Hour * PRSyncPeriod)
+		}
+
+	}
+}
+
+func (T *Table) SyncUser() error {
+
+	var err error
+	var res *github.Response
+	var currentPage, lastPage int
+	var wg sync.WaitGroup
+	var opt github.ListMembersOptions
+	var result, members []*github.User
+
+	currentPage = 1
+	lastPage = 1000
+	opt.PerPage = ItemsPerPage
+	opt.Page = currentPage
+	opt.PublicOnly = true
+
+	for currentPage <= lastPage {
+		wg.Add(1)
+		core.AddMsg(func() error {
+			members, res, err = cli.Organizations.ListMembers(ctx, HuaweiOrg, &opt)
+			wg.Done()
+			return err
+		})
+		wg.Wait()
+		if err != nil {
+			log.Printf("Table->SyncUser() Error getting user list err=%v", err)
+			return err
+		}
+		result = append(result, members...)
+		currentPage++
+		opt.Page = currentPage
+		lastPage = res.LastPage
+	}
+
+	for _, usr := range result {
+		userName := *usr.Login
+		if !T.Exist(userName) {
+			T.Add(NewRecord(userName, userName, 0))
+		}
+	}
+
+	return err
+
+}
+
 //Get the latest updated commit counts
-func (T *Table) SyncCommits () error {
+func (T *Table) SyncCommits() error {
 
 	var wg sync.WaitGroup
-
+	var reposInOrgs []*github.Repository
+	var err error
 
 	//Nullify the commit number for all the users
 	T.zeroCommits()
+	opt := &github.RepositoryListByOrgOptions{Type: "public"}
+	opt.PerPage = ItemsPerPage
 	//for Orgaizations
 	for _, org := range Organizations {
-		T.CallorWait()
-		opt := &github.RepositoryListByOrgOptions{Type: "public"}
+
 		//Get Maximum number of repos
-		opt.PerPage = 1000
-		repos , resp, err := cli.Repositories.ListByOrg(ctx, org, opt)
+		wg.Add(1)
+		core.AddMsg(func() error {
+			reposInOrgs, _, err = cli.Repositories.ListByOrg(ctx, org, opt)
+			wg.Done()
+			return err
+		})
+		wg.Wait()
 
-		for _, repo := range repos {
+		if err != nil {
+			log.Printf("Table->SyncCommits() Error collecting repos for org=%s err=%v", org, err)
+			return err
+		}
 
-			wg.Add(1)
+		for _, repo := range reposInOrgs {
 
-			GetContributions(resp)
-
-
+			contributors, err := T.GetContributors(org, *repo.Name)
+			if err != nil {
+				log.Printf("Table->SyncCommits() Error collecting contributors for org=%s repo=%s err=%v", org, repo.Name, err)
+				return err
+			}
+			//Update the contributors
+			for _, c := range contributors {
+				userName := *c.Login
+				if T.Exist(userName) {
+					T.Row[userName].NewCommits += c.GetContributions()
+					log.Printf("Table->SyncCommits() %s/%s/%s contrib=%d Total=%d", org, *repo.Name, userName, c.GetContributions(), T.Row[userName].NewCommits)
+				}
+			}
 		}
 	}
 	T.swapToCommits()
+	T.Update = time.Now()
 
-	return nil
+	return err
 }
 
-// CallorWait is a blocking call, which will first check if we have enough remaining HTTP calls we can make for this hour
-// other wise we will know when it gets reset, so we will wait (sleeping) until the re-set
-func (T *Table) CallorWait() {
+func (T *Table) GetContributors(orgName, repoName string) ([]*github.Contributor, error) {
 
-	T.UpdateRate()
+	var result, contribs []*github.Contributor
+	var err error
+	var res *github.Response
+	var currentPage, lastPage int
+	var wg sync.WaitGroup
+	var opt github.ListContributorsOptions
 
-	if T.RemaingCalls > 0 {
-		return
-	}
+	//Default the page numbers
+	currentPage = 1
+	lastPage = 1000
+	opt.PerPage = ItemsPerPage
+	opt.Page = currentPage
 
-	timetoWait := T.ResetTime.Sub(time.Now()).Seconds()
-	log.Printf("Exhaused all the calls for this hour, Waiting for %d", timetoWait)
+	for currentPage <= lastPage {
 
-	for timetoWait > 0 {
-		time.Sleep(time.Second)
-		timetoWait -= 1
-		if int(timetoWait) % 10 == 0 {
-			log.Printf("%dsecs more to retry..", timetoWait)
+		wg.Add(1)
+
+		core.AddMsg(func() error {
+			contribs, res, err = cli.Repositories.ListContributors(ctx, orgName, repoName, &opt)
+			wg.Done()
+			return err
+		})
+		wg.Wait()
+		currentPage++
+		opt.Page = currentPage
+		lastPage = res.LastPage
+		if err != nil {
+			return result, err
 		}
+		result = append(result, contribs...)
 	}
+	return result, err
 }
 
-//
-func (T *Table) UpdateRate () {
-
-	rate, _, err := cli.RateLimits(ctx)
-
-	if err != nil {
-		log.Printf("Unable to get Rate err=%v\n", err)
-		return
-	}
-
-	T.RemaingCalls = rate.Core.Remaining
-	T.ResetTime = rate.Core.Reset.Time
-}
-
-func NewTable () *Table {
+//NewTable creates a dummy
+func NewTable() *Table {
 	var T Table
 	T.Row = make(map[string]*Record)
 
 	return &T
 }
 
+type Record struct {
+	sync.Mutex
+	Name       string    //Name of the person
+	GHandle    string    //Github handle of this person
+	Commits    int       //Number of commits so far
+	NewCommits int       //Current processing number of commits
+	Updated    time.Time //Last updated at
+}
 
-
-func NewRecord (Name , GHandle string, commits int) *Record {
+func NewRecord(Name, GHandle string, commits int) *Record {
 	var R Record
 	R.Name = Name
 	R.GHandle = GHandle
@@ -161,62 +265,11 @@ func NewRecord (Name , GHandle string, commits int) *Record {
 	return &R
 }
 
-func sumStats (weeks []github.WeeklyStats) github.WeeklyStats{
-	var res github.WeeklyStats
-
-	res.Commits = new(int)
-	res.Additions = new(int)
-	res.Deletions = new(int)
-
-	for _, w := range weeks {
-		*res.Additions += *w.Additions
-		*res.Deletions += *w.Deletions
-		*res.Commits += *w.Commits
-	}
-	return res
-}
-
-func GetContributions (ctx context.Context, cli *github.Client, org, repo string) ([]*github.ContributorStats, *github.Response, error) {
-
-	retryCount := 3
-	var contribs []*github.ContributorStats
-	var res *github.Response
-	var err error
-
-	cli.Repositories.ListCommits()
-
-	for retryCount > 0 {
-		contribs, res, err = cli.Repositories.ListContributorsStats(ctx, org, repo)
-		if err != nil {
-			fmt.Printf("Error Occured %v\n", err)
-			if strings.Contains(err.Error(), "job scheduled on GitHub side; try again later") {
-				time.Sleep(time.Second * 30 )
-				retryCount -= 1
-				continue
-			}
-		}
-		if res.Remaining == 0 {
-			//No more retry available, we have exausted the number of retries for this session
-			waitSeconds := res.Reset.Second()
-			for waitSeconds > 0 {
-				time.Sleep(time.Second)
-				fmt.Fprintf(os.Stderr, "Will retry in %dsecs\n", waitSeconds)
-				waitSeconds -= 1
-			}
-			continue
-		}
-		break
-	}
-
-
-	return contribs, res, err
-}
-
 func init() {
 
 	ctx = context.Background()
 	cli = github.NewClient(nil)
-	core = NewGitWorker(ctx,cli,"Core", true)
+	core = NewGitWorker(ctx, cli, "Core", true)
 	search = NewGitWorker(ctx, cli, "Search", false)
 
 	go core.ProcessWorkItem()
@@ -224,61 +277,29 @@ func init() {
 
 }
 
-func main () {
+func mainfunc() {
 
-//	userName := "dhilipkumars"
-	orgName := "kubernetes"
-	repoName := "kubernetes"
+	T := NewTable()
 
+	//Start the go sync table
+	go T.Sync()
 
-
-
-/*
-	contribs, res, err := GetContributions(ctx, cli, orgName, repoName)
-	fmt.Printf("Response res=%v firstPage=%d page=%d lastpage=%d \n", *res, res.FirstPage, res.NextPage, res.LastPage)
-	if err != nil {
-		fmt.Printf("Obtained error =%v\n", err)
-		return
-	}
-
-	fmt.Printf("Total Contributors for this repo %s  is %d\n", repoName, len(contribs))
-	for _ , c := range contribs {
-		fmt.Printf("User:%s, Total=%d Stats=%v\n",*c.Author.Login, *c.Total, sumStats(c.Weeks))
-	}
-
-	*/
-
-
-	////opt := github.ListOptions{PerPage:10000}
-	var opt github.ListContributorsOptions
-	opt.PerPage = 10000
-
-	var cpage, lpage int
-	var finContributors []*github.Contributor
-
-	cpage = 0
-	lpage = 1000
-
-	for cpage < lpage {
-
-		// cli.Organizations.ListMembers()
-		contributors, cres, cerr := cli.Repositories.ListContributors(ctx, orgName, repoName, &opt)
-		fmt.Printf("Response res=%v\n", *cres)
-		if cerr != nil {
-			fmt.Printf("Obtained error =%v\n", cerr)
-			return
+	for {
+		select {
+		case <-time.After(time.Minute):
+			recs := T.Tabulate()
+			tableStr := fmt.Sprintf("====================Updated %v==============\n", T.Update)
+			for i, rec := range recs {
+				tableStr += fmt.Sprintf("%3d) %5s\t\t%d\t%d\n", i+1, rec.GHandle, rec.Commits, rec.NewCommits)
+			}
+			tableStr += fmt.Sprintf("=============================================\n")
+			log.Println(tableStr)
 		}
-		fmt.Printf("There are more pages to fetch %d\n", lpage - cpage)
-		lpage = cres.LastPage
-		cpage = cres.NextPage
-		opt.Page = cpage
-		time.Sleep(time.Millisecond * 100)
-		finContributors = append(finContributors, contributors...)
 	}
-	fmt.Printf("Total Contributors for this repo %s  is %d\n", repoName, len(finContributors))
-	for _ , c := range finContributors {
+}
 
-		fmt.Printf("User:%s, Total=%d\n",*c.Login, c.GetContributions())
-	}
+func main() {
+
+	mainfunc()
 
 }
--- worker.go
+++ worker.go
@@ -7,8 +7,14 @@ import (
 	"log"
 	"time"
 	"context"
-
 )
+
+type Msg struct {
+	T  time.Time
+	fn func() error
+}
+
+
 
 //GitWorker a Simple workquie executor
 type GitWorker struct {
@@ -26,6 +32,11 @@ func NewGitWorker (ctx context.Context, cli *github.Client, Name string, core bo
 	return &GitWorker{MsgProcessed:0, SleepInterval:10, Name: Name, Ctx:ctx, Cli:cli, Q:workqueue.NewNamed(Name), isCore:core}
 }
 
+func (W *GitWorker) AddMsg(fn func() error)  {
+	msg := &Msg{fn:fn, T:time.Now()}
+	W.Q.Add(msg)
+}
+
 func (W *GitWorker) ProcessWorkItem () {
 
 	log.Printf("%s Worker - Started", W.Name)
@@ -33,20 +44,13 @@ func (W *GitWorker) ProcessWorkItem () {
 
 	for {
 
-		W.Limit = nil
-
-		item, shutdown := W.Q.Get()
-		if shutdown {
-			log.Printf("%s Worker - Recived shutdown signal", W.Name)
-			return
-		}
+		//W.Limit = nil
 		rL, _, err := W.Cli.RateLimits(W.Ctx)
 		if err != nil {
-			log.Printf("RateLimits() error =%v", err)
-			time.Sleep(time.Second)
+			log.Printf("RateLimits() error =%v QLen=%d", err, W.Q.Len())
+			time.Sleep(time.Second * W.SleepInterval)
 			continue
 		}
-
 		if W.isCore {
 			W.Limit = rL.Core
 		} else {
@@ -54,18 +58,25 @@ func (W *GitWorker) ProcessWorkItem () {
 		}
 
 		if W.Limit.Remaining == 0 {
-			log.Printf("%s Worker - No remaining calls available, resets at %v", W.Name, W.Limit.Reset.Time)
+			log.Printf("%s Worker - No remaining calls available, resets at %v QLen=%d", W.Name, W.Limit.Reset.Time, W.Q.Len())
 			time.Sleep(time.Second * W.SleepInterval)
 			continue
 		}
 
+		item, shutdown := W.Q.Get()
+		if shutdown {
+			log.Printf("%s Worker - Recived shutdown signal", W.Name)
+			return
+		}
+		msg := item.(*Msg)
+		log.Printf("%s Worker - Recived Msg Remaining=%v MsgTimeStamp=%s", W.Name, W.Limit, msg.T.String())
+
 		W.MsgProcessed++
-		fn := item.(func() error)
-		err = fn()
+		err = msg.fn()
 		if err != nil {
 			log.Printf("%s Worker - Error processign message=%v", W.Name, err)
 		}
-		W.Q.Done(fn)
+		W.Q.Done(msg)
 
 	}
 
